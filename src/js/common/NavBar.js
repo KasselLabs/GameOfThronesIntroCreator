bd33e284062f8394e373d8063234781e4daeb3b9
BrOrlandi <brorlandi@gmail.com>
2019-07-21 00:02:47 -0300

    Update links to Game of Thrones

--- src/js/common/NavBar.js
+++ src/js/common/NavBar.js
@@ -19,12 +19,12 @@ class NavBar extends Component {
     const menu = (
       <ul className="menu">
         <li><a href="/#/">HOME</a></li>
-        <li><a href="https://help.kassellabs.io/westworld/">CONTACT</a></li>
+        <li><a href="https://help.kassellabs.io/game-of-thrones/">CONTACT</a></li>
         <li>
           <a>ABOUT</a> {/* eslint-disable-line jsx-a11y/anchor-is-valid */}
           <ul className="dropdown">
-            <li><a href="https://help.kassellabs.io/westworld/">FAQ</a></li>
-            <li><a href="https://help.kassellabs.io/westworld/#termsOfService">TERMS OF SERVICE</a></li>
+            <li><a href="https://help.kassellabs.io/game-of-thrones/">FAQ</a></li>
+            <li><a href="https://help.kassellabs.io/game-of-thrones/#termsOfService">TERMS OF SERVICE</a></li>
             <li><a href="https://help.kassellabs.io/privacy/">PRIVACY POLICY</a></li>
           </ul>
         </li>
