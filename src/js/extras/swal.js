883bfd9289bb05d7d3783f201636f5be1f132a5e
BrOrlandi <brorlandi@gmail.com>
2019-07-23 01:00:10 -0300

    Fix modal styles.

--- src/js/extras/swal.js
+++ src/js/extras/swal.js
@@ -2,7 +2,7 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 import Swal from 'sweetalert2';
 
 const MySwal = Swal.mixin({
-  customClass: 'westworld-sweetalert',
+  customClass: 'kassel-app-sweetalert',
 });
 
 export default MySwal;
