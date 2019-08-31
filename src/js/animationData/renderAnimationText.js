fef8ea2f110c50ca8b2996c3251c8be40f3980de
BrOrlandi <brorlandi@gmail.com>
2019-08-31 19:21:55 -0300

    Improve dynamic logo font sizing.

--- src/js/animationData/renderAnimationText.js
+++ src/js/animationData/renderAnimationText.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
+const isLetter = char => /[a-z]/i.test(char);
+
+const isUpperCase = char => char.toUpperCase() === char && isLetter(char);
+
+const isLowerCase = char => char.toLowerCase() === char && isLetter(char);
+
+const wordHasUpperCase = word => word.split('').some(char => isUpperCase(char));
+
+const renderWordPart = (wordPart, key, className) => (
+  <span key={key} className={className}>{wordPart}</span>
+);
+
+const isSameCase = (a, b) =>
+  (isUpperCase(a) && isUpperCase(b))
+  || (isLowerCase(a) && isLowerCase(b));
+
 const renderLogoWord = (word, index, array) => {
   const arrayLength = array.length;
   const { length } = word;
@@ -7,24 +23,75 @@ const renderLogoWord = (word, index, array) => {
   const isFirstWord = 0 === index;
   const isLastWord = index === arrayLength - 1;
   const isWordLengthGreaterThan3 = length > 3;
-  const shouldCaptilizeWord = isFirstWord || isLastWord || isWordLengthGreaterThan3;
-  if (shouldCaptilizeWord) {
-    const capital = word[0];
-    const restOfWord = word.slice(1);
-    return [
-      (
-        <span key={`0${index}`} className="capitilize">{capital}</span>
-      ),
-      (
-        <span key={`1${index}`} className="rest-of-word">{restOfWord}</span>
-      ),
-    ];
+  const hasUpperCase = wordHasUpperCase(word);
+  if (
+    !(isWordLengthGreaterThan3
+      || hasUpperCase
+      || isFirstWord
+      || isLastWord)
+  ) {
+    return renderWordPart(word, `sw${index}`, 'small-text');
   }
 
-  // if (!isWordLengthGreaterThan3) {
-  return (
-    <span key={`2${index}`} className="small-text">{word}</span>
-  );
+  if (!hasUpperCase) {
+    return renderWordPart(word, `rw${index}`, 'rest-of-word');
+  }
+
+  const UPPERCASE = 1;
+  const LOWERCASE = 2;
+  const firstCharIsUpperCase = isUpperCase(word[0]);
+
+  let findingType = firstCharIsUpperCase ? UPPERCASE : LOWERCASE;
+  let lastBreakCharAt = 0;
+  let wordParts = [];
+  const lastChar = word[length - 1];
+  const isLastCharsSameCase = isSameCase(word[length - 2], lastChar);
+  for (let i = 1; i < length; i += 1) {
+    const char = word[i];
+    const isLastChar = i === length - 1;
+    const isFindingUpper = findingType === UPPERCASE;
+    const isFindingLower = findingType === LOWERCASE;
+
+    const matchUpper = findingType === UPPERCASE && isUpperCase(char);
+    const matchLower = findingType === LOWERCASE && isLowerCase(char);
+
+    if ((matchUpper || matchLower) && !isLastChar) {
+      // eslint-disable-next-line no-continue
+      continue;
+    }
+
+    const breakAt = isLastChar && isLastCharsSameCase ? i + 1 : i;
+    const wordPart = word.slice(lastBreakCharAt, breakAt);
+
+    let wordRendered = null;
+    if (isFindingUpper) {
+      wordRendered = renderWordPart(wordPart, `wpc${index}-${i}`, 'capitalize');
+      findingType = LOWERCASE;
+    }
+
+    if (isFindingLower) {
+      wordRendered = renderWordPart(wordPart, `wpr${index}-${i}`, 'rest-of-word');
+      findingType = UPPERCASE;
+    }
+    lastBreakCharAt = i;
+    wordParts = wordParts.concat(wordRendered);
+  }
+
+  if (!isLastCharsSameCase) {
+    let wordRendered = null;
+    if (isUpperCase(lastChar)) {
+      wordRendered = renderWordPart(lastChar, `wpc${index}l`, 'capitalize');
+      findingType = LOWERCASE;
+    }
+
+    if (isLowerCase(lastChar)) {
+      wordRendered = renderWordPart(lastChar, `wpr${index}l`, 'rest-of-word');
+      findingType = UPPERCASE;
+    }
+    wordParts = wordParts.concat(wordRendered);
+  }
+
+  return wordParts;
 };
 
 export const renderLogoText = (text) => {
@@ -36,14 +103,16 @@ export const renderLogoText = (text) => {
     const isNotLastWord = i !== words.length - 1;
 
     if (isNotLastWord) {
-      textParts = textParts.concat(<span key={`s${i}`}>{' '}</span>);
+      textParts = textParts.concat(renderWordPart(' ', `s${i}`));
     }
   }
-
-  const fontSize = 2.5 - (0.08 * (text.length - 15));
+  const minFontSize = 1.7;
+  const fontSizeByLength = 2.5 - (0.08 * (text.length - 15));
+  const fontSizeMinOrCalculated = fontSizeByLength > minFontSize
+    ? fontSizeByLength : minFontSize;
 
   return (
-    <div className="rendered-logo" style={{ fontSize: `${fontSize}em` }}>
+    <div className="rendered-logo" style={{ fontSize: `${fontSizeMinOrCalculated}em` }}>
       {textParts}
     </div>
   );
