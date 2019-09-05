7d9e02e0a4f2511da910b1b87a72df9e388db23b
BrOrlandi <brorlandi@gmail.com>
2019-09-05 15:27:29 -0300

    Apply logo-timing-2 branch.

--- changeTime.sh
+++ changeTime.sh
@@ -1,3 +1,3 @@
 sed -i -e "s/export const TIME_FACTOR = .*;/export const TIME_FACTOR = $1;/g" src/js/api/config.js
 sed -i -e "s/\$TIME_FACTOR = .*;/\$TIME_FACTOR = $1;/g" src/styles/animations.styl
-#git apply ./renderer.patch
+git apply ./renderer.patch
