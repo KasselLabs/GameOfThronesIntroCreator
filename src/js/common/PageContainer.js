1f562a6a87854bdaafbe96326a2f89781bf704a2
Nihey Takizawa <nihey.takizawa@gmail.com>
2019-09-23 10:20:51 -0300

    Fix ProductHunt link to cover noopener and noreferer

--- src/js/common/PageContainer.js
+++ src/js/common/PageContainer.js
@@ -20,7 +20,7 @@ const PageContainer = ({ children }) => (
       <a href="https://twitter.com/share?url=https://gameofthronesintrocreator.kassellabs.io/&amp;text=Check%20out%20the%20Game%20of%20Thrones%20Intro%20Creator!&amp;hashtags=GameOfThrones,KasselLabs" target="_blank" rel="noopener noreferrer">
         <img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" width="40" />
       </a>
-      <a href="https://www.producthunt.com/posts/game-of-thrones-intro-creator?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-game-of-thrones-intro-creator" target="_blank">
+      <a href="https://www.producthunt.com/posts/game-of-thrones-intro-creator?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-game-of-thrones-intro-creator" target="_blank" rel="noopener noreferrer">
         <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=168012" alt="Game of Thrones Intro Creator - Create a Game of Thrones intro video | Product Hunt Embed" style={{ width: '191px', height: '41px' }} width="250px" height="54px" />
       </a>
     </div>
